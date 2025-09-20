088379c18a7672fd1cc320dbb9b76bc1330dcce8
ay001-web <ayushyadav2250@gmail.com>
2025-09-20 20:00:36 +0530

    Date and Time in JS

---
+++ 01_basics/07_DatesInJs.js
@@ -0,0 +1,25 @@
+// Date 
+
+let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+//let myCreatedDate = new Date(2023,0,23)
+let myCreatedDate = new Date("2023-01-23")
+//console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+console.log(newDate.getDate());
+
+
+
+
